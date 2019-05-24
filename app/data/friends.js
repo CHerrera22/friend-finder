3341161c4af0941887b6a64943113e8c3272f75e
Cris <cherrera22@gmail.com>
2019-05-23 19:23:29 -0700

    blah

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+//Les Friend Matches
+
+var friends = [
+    {
+        name: "Bette Midler",
+        photo: "https://mix96sac.com/wp-content/uploads/sites/4/2018/10/GettyImages-618903238-740x1112.jpg",
+        scores: [4, 3, 4, 2, 1, 2, 3, 5, 4, 2],
+    },
+    {
+        name: "Gloria Estefan",
+        photo: "https://i.pinimg.com/originals/d9/f0/c4/d9f0c4d8217b069cec76cd442c7df4c1.jpg",
+        scores: [1, 5, 2, 4, 3, 3, 5, 1, 2, 4],
+    },
+    {
+        name: "Ice-T",
+        photo: "https://i.pinimg.com/originals/d5/73/cb/d573cbf023c000f7d979696432b0ce7d.jpg",
+        scores: [5, 4, 5, 4, 5, 3, 2, 1, 5, 4],
+    },
+    {
+        name: "Pikachu",
+        photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/12/POTY_Pikachu_3.jpg",
+        scores: [1, 2, 1, 2, 1, 2, 1, 3, 4, 5],
+    },
+    {
+        name: "Raffi",
+        photo: "https://www.thebeaverton.com/wp-content/uploads/2017/12/raffi.jpg",
+        scores: [5, 4, 3, 5, 3, 1, 4, 4, 3, 5,],
+    },
+];
+
+module.exports = friends;
---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+//Les HTML Routes
+
+var path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+      });
+    
+      app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
+    
+      //default
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
+    
+};
